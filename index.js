21c2f802479d9a075a53ef1131ec1857e5bae422
Bolaji oluwatobi <bolajioluwatobi249@gmail.com>
2024-03-11 19:26:46 +0100

    interactiveproject

---
+++ index.js
@@ -0,0 +1,38 @@
+// let fourthButton = document.getElementById('btn4')
+// console.log(fourthButton,'this is the fouth button')
+
+// function selectNumber(num){
+//     console.log(num)
+//     fourthButton.style.backgroundColor = 'hsl(216, 12%, 54%)'
+// }
+// function saveNumber(){
+//     fourthButton.style.color = 'red'
+// }
+// let fourthButton = document.getElementsByClassName('btn4')
+// console.log(fourthButton,'this is the fouth button')
+
+// function selectNumber(num){
+//     console.log(num)
+//     fourthButton.style.backgroundColor = 'white'
+// }
+// function selectNumber(num){
+//     for (let i = 0; i < fourthButton.length; i++) {
+//         let element = fourthButton[i];
+//         element.style.backgroundColor = 'hsl(216, 12%, 54%)'
+//       }
+      
+//     }
+    // let fifthButton = document.getElementsByClassName('btn5')
+    // console.log(fifthButton,'this is the fifth button')
+    
+    // function selectNumber(nin){
+    //     console.log(nin)
+    //     fifthButton.style.backgroundColor = 'white'
+    // }
+    // function selectNumber(nin){
+    //     for (let i = 0; i < fifthButton.length; i++) {
+    //         let element = fifthButton[i];
+    //         element.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    //       }
+          ``
+    //     }
